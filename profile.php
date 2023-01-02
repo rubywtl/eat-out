e862b23daeee70367e145703a9ef3c48298e24c1
rubywtl <51363341+rubywtl@users.noreply.github.com>
2023-01-02 11:03:23 -0800

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+
+
+<header>
+	<title>Eatout</title>
+</header>
+
+<body>
+	<link rel="stylesheet" href="profilestyle.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	
+	<nav>
+		<div class="logo">
+			<h3>Eatout<h3>
+		</div>
+		<ul  class="nav-links">
+			<li><a href="home.php">home</a></li>
+			<li><a href="calorie.php">choose</a></li>
+			<li><a href="about.html">about</a></li>
+			<li><a href="profile.php">account</a></li>
+		</ul>
+	</nav>
+	
+	<div class="sidenav">
+	<a href="settings.php">settings</a>
+	<a href="saved.php">saved</a>
+	</div>
+
+	<div class="main">
+		<h5>what you can do as a member</h5>
+		<p>
+		1. see saved sets<br>
+		2. analysis on calories<br>
+		</p>
+	</div>
+	
+</body>
+
+</html>
